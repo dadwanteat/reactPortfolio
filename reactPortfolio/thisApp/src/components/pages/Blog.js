8e9d17a0c6f0c35215d042ec1c8751c238db1aac
Robert Dalton <dalton.robertw@gmail.com>
2020-11-22 00:01:16 -0800

    finalized for now

--- reactPortfolio/thisApp/src/components/pages/Blog.js
+++ reactPortfolio/thisApp/src/components/pages/Blog.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ImgB from './denimbackground.png';
+import RdMe from './ReadMeGen.png';
+import Rsp from './Responsive.png';
+import StrTrk from './StarTrekQuiz.png';
+import StrTrkSmpl from './StarTrekSample.png';
 
 function Blog() {
   return (
@@ -7,18 +11,19 @@ function Blog() {
       <br/>
       <h1>Projects</h1>
       <br/>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <br/>
+      <img src={RdMe}  width="500" alt="ReadMe Screen Shot"/>
+      <p>Above is a command-line generated ReadMe sample</p>
+
+      <img src={StrTrk}  width="500" alt="ReadMe Screen Shot"/>
+
+      <img src={StrTrkSmpl}  width="500" alt="ReadMe Screen Shot"/>
+      <p>Above are two screen shots of a Star Trek Quiz that keeps your score!</p>
+
+      <img src={Rsp}  width="800" alt="ReadMe Screen Shot"/>
+      <p>Above is a my first Website! Memories...</p>
+      <br/>
+      <br/>
     </div>
   );
 }
